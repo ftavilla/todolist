18691ee1b39b630f0a6f91276450ba843462cb9e
ftavilla <fabrice.tavilla@gmail.com>
2013-12-22 11:29:48 +0100

    Unesseary comments removed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = module.exports = express();
 var port;
 
-// Check node_env, if not set default to development
 process.env.NODE_ENV = (process.env.NODE_ENV || "development");
 
 var createToDoList = function(req, res,  next) {
@@ -26,9 +25,6 @@ app.configure(function(){
     app.use(app.router);
 });
 
-/*
- * This section is for environment specific configuration
- */
 app.configure('development', function(){
     port = 8080;
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -43,9 +39,5 @@ app.listen(port, function(){
     console.log("Express server listening on port %s in %s mode", port, app.settings.env);
 });
 
-/*
- * Exports the express app for other modules to use
- * all route matches go the routes.js file
- */
 module.exports.app = app;
 routes = require('./routes');
--- controllers/todo.js
+++ controllers/todo.js
@@ -1,9 +1,7 @@
-/* On affiche la todolist et le formulaire */
 exports.home = function(req, res) {
     res.render('todoList.ejs', {todolist: req.session.todolist});
 };
 
-    /* On ajoute un élément à la todolist */
 exports.add = function(req, res) {
     if (req.body.newtodo != '') {
         req.session.todolist.push(req.body.newtodo);
@@ -11,7 +9,6 @@ exports.add = function(req, res) {
     res.redirect('/todo');
 };
 
-/* Supprime un élément de la todolist */
 exports.delete = function(req, res) {
     if (req.params.id != '') {
         req.session.todolist.splice(req.params.id, 1);
